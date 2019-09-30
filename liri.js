0d9ddeb07c54dbfdac96cd07a6e7126610c81258
Julian <cusco2891@gmail.com>
2019-09-30 17:01:12 -0400

    pseudocode

---
+++ liri.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+<artist/band name here>
+
+concert-this    <artist/band name here>
+
+// events api search
+("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+
+// renders back in console
+Name of the venue
+
+Venue location
+
+Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+
+
+spotify-this-song
+
+movie-this
+
+do-what-it-says
+
